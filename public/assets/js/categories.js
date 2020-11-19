a3c47603e9d38c43ad507e3a672498319942f972
jespi116 <jespi116@fiu.edu>
2020-11-19 18:15:00 -0500

    categories.js updated

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -2,12 +2,20 @@ async function addFormHandler(event) {
     event.preventDefault();
 
     const user_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
-    const quant = document.querySelector('#quant').value;
+    let quant = document.querySelector('#quant').value;
     const product_id = document.querySelector('#pid').textContent.split(':')[1];
+
+    if(!quant){
+        quant = 1;
+    }
 
     console.log(user_id, quant, product_id)
 
-    
+    if(user_id === '0'){
+        document.location.replace('/login');
+    }
+    else{
+        
         const response = await fetch(`/api/mycart`, {
             method: 'post',
             body: JSON.stringify({
@@ -21,11 +29,11 @@ async function addFormHandler(event) {
           console.log(response)
   
           if (response.ok) {
-            //document.location.replace('/myCart');
+            document.location.replace('/myCart');
           } else {
             alert(response.statusText);
           }
-    
+    }
     
 }
 
