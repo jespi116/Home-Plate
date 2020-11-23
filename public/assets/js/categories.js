7db4582dd3ccc99ca6e277ad344d07e8565b1630
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-11-22 20:05:52 -0500

    AAdded Products to MyCart

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -1,26 +1,41 @@
+
+async function isProductExist(user_id, product_id){
+
+  const response = await fetch(`/api/mycart/user`, {
+    method: 'post',
+    body: JSON.stringify({
+      user_id,
+      product_id       
+    }),
+   headers: { 'Content-Type': 'application/json' }
+  });
+  
+  const data = await response.json();
+
+  if (data !== null) {
+    return true
+  } else {
+    return false
+  }
+
+}
 async function addFormHandler(product_id, quantity) {
-    //event.preventDefault();
 
-    //const user_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
-   const user_id = document.querySelector("#user").getAttribute("attr");
-   //let quant = document.querySelector('#quant').value;
-    /*const product_id = document.querySelector('#pid').textContent.split(':')[1]; */
+  const user_id = document.querySelector("#user").getAttribute("attr");
 
-    alert("User ID: "+ user_id);
-    alert("Product ID: "+ product_id);
-    alert("Quantity: "+ quantity)
+    const val =  isProductExist(user_id.toString(), product_id.toString());
+
     if(!quant){
         quant = 1;
     }
-    console.log("\n====================================\n");
-    console.log(user_id, quant, product_id);
-    console.log("\n====================================\n");
 
     if(user_id === '0'){
         document.location.replace('/login');
     }
     else{
-        
+
+      if(!(await val).valueOf()){
+
         const response = await fetch(`/api/mycart`, {
             method: 'post',
             body: JSON.stringify({
@@ -31,15 +46,12 @@ async function addFormHandler(product_id, quantity) {
            headers: { 'Content-Type': 'application/json' }
           });
   
-          console.log(response)
-  
           if (response.ok) {
             document.location.replace('/myCart');
           } else {
             alert(response.statusText);
           }
+        }
     }
     
 }
-
-//document.querySelector('.add-form').addEventListener('submit', addFormHandler);
