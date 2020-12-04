7051ad0de7a49fe4e9e7e0028f0293ddec40b0e4
jespi116 <jespi116@fiu.edu>
2020-12-04 04:36:34 -0500

    small tweak to categories style

--- public/assets/js/categories.js
+++ public/assets/js/categories.js
@@ -1,39 +1,43 @@
 async function addFormHandler(product_id, $this) {
-    //event.preventDefault();
+  //event.preventDefault();
 
-    
-   const user_id = document.querySelector("#user").getAttribute("attr");
-   let quant = $this.previousElementSibling.value;
-   
-
-    
-    if(!quant){
-        quant = 1;
-    }
-
-    if(!user_id){
-        document.location.replace('/login');
-    }
-    else{
-
-      if(!(await val).valueOf()){
-
-        const response = await fetch(`/api/mycart`, {
-            method: 'post',
-            body: JSON.stringify({
-              user_id,
-              product_id,
-              quantity: quant       
-            }),
-           headers: { 'Content-Type': 'application/json' }
-          });
   
-          if (response.ok) {
-            document.location.replace('/myCart');
-          } else {
-            alert(response.statusText);
-          }
+ const user_id = document.querySelector("#user").getAttribute("attr");
+ let quant = $this.previousElementSibling.value;
+ 
+
+  
+  if(!quant){
+      quant = 1;
+  }
+  console.log("\n====================================\n");
+  console.log(user_id, quant, product_id);
+  console.log("\n====================================\n");
+
+  if(!user_id){
+      document.location.replace('/login');
+  }
+  else{
+      
+      const response = await fetch(`/api/mycart`, {
+          method: 'post',
+          body: JSON.stringify({
+            user_id,
+            product_id,
+            quantity: quant       
+          }),
+         headers: { 'Content-Type': 'application/json' }
+        });
+
+        console.log(response)
+
+        if (response.ok) {
+          document.location.replace('/myCart');
+        } else {
+          alert(response.statusText);
         }
-    }
-    
+  }
+  
 }
+
+//document.querySelector('.add-form').addEventListener('submit', addFormHandler);
